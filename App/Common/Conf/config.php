3ae408d7726d0ecfdcd33a992cecd2c426171e6f
mchenyuxiang <mchenyuxiang@foxmail.com>
2016-12-23 16:12:13 +0800

    修改数据库配置

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -1,4 +1,7 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    
+    //加载其他配置文件
+    'LOAD_EXT_CONFIG'      => 'db', //加载扩展配置文件
 );
---
+++ App/Common/Conf/db.php
@@ -0,0 +1,17 @@
+<?php?>
+/**
+ * Created by PhpStorm.
+ * User: mchenyuxiang
+ * Date: 2016/12/23
+ * Time: 16:10
+ */
+return array (
+    'DB_TYPE' => 'mysqli',
+    'DB_HOST' => '127.0.0.1',
+    'DB_PORT' => '3306',
+    'DB_USER' => 'root',
+    'DB_PWD' => '',
+    'DB_NAME' => 'diandang',
+    'DB_PREFIX' => 'zzcms_',
+    'DB_CHARSET' => 'utf8',
+);
