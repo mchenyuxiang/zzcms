d1652e1678bcfc3e32fa34c441ac31d941590ffb
mchenyuxiang <csyxchen@qq.com>
2016-12-25 15:12:00 +0800

    树形表格初步添加成功，还需要再次调试

---
+++ App/Admin/Controller/MenuController.class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuxiang
+ * Date: 2016/12/25
+ * Time: 11:06
+ * Description: 后台菜单管理
+ */
+namespace Admin\Controller;
+
+use Think\Controller;
+
+class MenuController extends Controller{
+    
+    public function index(){
+        $this->display();
+    }
+
+    public function tree1(){
+        $this->display();
+    }
+}
+?>
+
