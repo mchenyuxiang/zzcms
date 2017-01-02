78b7c53b4646428444253b3ad1bd40b36b2b16da
mchenyuxiang <csyxchen@qq.com>
2017-01-02 23:05:04 +0800

    文章内容添加开始制作

---
+++ App/Admin/Controller/ContentController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuxiang
+ * Date: 2017/1/2
+ * Time: 22:58
+ * Description: 内容Controller
+ */
+namespace Admin\Controller;
+
+use Think\Controller;
+
+class ContentController extends Controller{
+    public function index(){
+        $this->display();
+    }
+    
+    public function add(){
+        $this->display();
+    }
+}
+?>
+
