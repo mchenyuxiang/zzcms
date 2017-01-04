1584d00d1572c34057d16d7070986eebced6d984
mchenyuxiang <csyxchen@qq.com>
2017-01-04 23:26:53 +0800

    职位列表制作

---
+++ App/Admin/Controller/OfficeController.class.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuxiang
+ * Date: 2017/1/4
+ * Time: 23:18
+ * Description: 员工相关控制器
+ */
+namespace Admin\Controller;
+
+use Think\Controller;
+
+class OfficeController extends Controller{
+    
+    public function list(){
+        $this->display();
+    }
+}
+?>
+
