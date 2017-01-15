87ab3a97a37cd708492839c4c124d308128cf541
mchenyuxiang <csyxchen@qq.com>
2017-01-15 22:56:39 +0800

    排班样式

---
+++ App/Admin/Controller/PlanController.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuxiang
+ * Date: 2017/1/15
+ * Time: 14:42
+ * Description: 人员排班
+ */
+namespace Admin\Controller;
+
+use Think\Controller;
+
+class PlanController extends Controller{
+    public function index(){
+        $this->display();
+    }
+}
+?>
+
