e74516388cf109db1dc49b62adf1b7ddac91a73b
mchenyuxiang <csyxchen@qq.com>
2017-01-18 02:06:32 +0800

    排班列表完成

--- App/Admin/Controller/PlanController.class.php
+++ App/Admin/Controller/PlanController.class.php
@@ -8,10 +8,36 @@
  */
 namespace Admin\Controller;
 
+use Common\Lib\Page;
 use Think\Controller;
 
 class PlanController extends Controller{
-    public function index(){
+    public function planList(){
+
+        $data = array();
+        $page = $_REQUEST['p'] ? $_REQUEST['p'] : 1;
+        $pageSize = $_REQUEST['pageSize'] ? $_REQUEST['pageSize'] : 15;
+
+        $offset = ($page - 1) * $pageSize;
+        $today = date("Y-m-d");
+        $data['endDay'] = array('egt',$today);
+        $cate = M('plan')
+            ->where($data)->limit($offset, $pageSize)->select();
+        $cateCount = M('employee')->where($data)->count();
+
+        $res = new Page($cateCount, $pageSize);
+        $pageRes = $res->show();
+        $this->assign('cate', $cate);
+        $this->assign('type',"人员排班");
+        $this->assign('page',$pageRes);
+        $this->display();
+    }
+    
+    public function add(){
+        $this->display();
+    }
+    
+    public function edit(){
         $this->display();
     }
 }
