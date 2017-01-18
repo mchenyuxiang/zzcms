31312e64d14fa2f3d3b06388f3d309f36a0f0055
mchenyuxiang <csyxchen@qq.com>
2017-01-19 03:11:43 +0800

    菜单翻页有bug

--- App/Admin/Common/function.php
+++ App/Admin/Common/function.php
@@ -7,19 +7,60 @@
  * Description:  通用方法
  */
 
-    function show_tip($status, $message,$data=array(),$url=''){
-        $result = array(
-            'status' => $status,
-            'message' => $message,
-            'data' => $data,
-            'url' => $url,
-        );
+function show_tip($status, $message, $data = array(), $url = '')
+{
+    $result = array(
+        'status' => $status,
+        'message' => $message,
+        'data' => $data,
+        'url' => $url,
+    );
 
-        exit(json_encode($result));
-    }
+    exit(json_encode($result));
+}
 
-    function getMd5Password($password){
-       return md5($password.'chenyuxiang'); 
+function getMd5Password($password)
+{
+    return md5($password . 'chenyuxiang');
+}
+
+/**
+ * 导出数据为excel表格
+ * @param $data    一个二维数组,结构如同从数据库查出来的数组
+ * @param $title   excel的第一行标题,一个数组,如果为空则没有标题
+ * @param $filename 下载的文件名
+ * @examlpe
+$stu = M ('User');
+ * $arr = $stu -> select();
+ * exportexcel($arr,array('id','账户','密码','昵称'),'文件名!');
+ */
+function exportexcel($data = array(), $title = array(), $filename = 'report')
+{
+    header("Content-type:application/octet-stream");
+    header("Accept-Ranges:bytes");
+    header("Content-type:application/vnd.ms-excel");
+    header("Content-Disposition:attachment;filename=" . $filename . ".xls");
+    header("Pragma: no-cache");
+    header("Expires: 0");
+    //导出xls 开始
+    if (!empty($title)) {
+        foreach ($title as $k => $v) {
+            $title[$k] = iconv("UTF-8", "GB2312", $v);
+        }
+        $title = implode("\t", $title);
+        echo "$title\n";
     }
+    if (!empty($data)) {
+        foreach ($data as $key => $val) {
+            foreach ($val as $ck => $cv) {
+                $data[$key][$ck] = iconv("UTF-8", "GB2312", $cv);
+            }
+            $data[$key] = implode("\t", $data[$key]);
+
+        }
+        echo implode("\n", $data);
+    }
+}
+
 ?>
 
--- App/Admin/Controller/ReportController.class.php
+++ App/Admin/Controller/ReportController.class.php
@@ -10,10 +10,31 @@ namespace Admin\Controller;
 
 use Think\Controller;
 
-class ReportController extends Controller{
-    public function index(){
+class ReportController extends Controller
+{
+    public function index()
+    {
         $this->display();
     }
+
+    public function personalDetail()
+    {
+        if ($_POST) {
+
+        } else {
+
+            $month=array();
+            for($i=1;$i<13;$i++){
+                $month[$i]['month']=$i;
+            }
+            $employeeName=M('employee')->select();
+            $this->assign('employeeName',$employeeName);
+            $this->assign('month',$month);
+            $this->assign('type','个人明细报表');
+            $this->display();
+        }
+    }
 }
+
 ?>
 
