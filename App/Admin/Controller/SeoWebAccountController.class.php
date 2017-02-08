6c9d07a0778bacf2c47cc42d244d9977a7342263
chenyuxiang <csyxchen@qq.com>
2017-02-08 17:06:33 +0800

    seo用户密码修改完成

--- App/Admin/Controller/SeoWebAccountController.class.php
+++ App/Admin/Controller/SeoWebAccountController.class.php
@@ -63,7 +63,43 @@ class SeoWebAccountController extends CommonController
      */
     public function UpdateSecrect()
     {
-        $this->display();
+        if ($_POST) {
+
+            $data = I('post.','');
+            $condition = array('id'=>$data['id']);
+            $userinfo = M('admin')->where($condition)->select();
+            $oldpassword = $userinfo[0]['password'];
+
+            if($oldpassword != getMd5Password($data['oldpassword'])){
+                return show_tip(0,'输入原密码错误');
+            }
+
+            if(strlen($data['newpassword']) < 6 && $data['id'] != 1){
+                return show_tip(0,'密码长度必须大于6');
+            }
+
+            if($data['newpassword'] != $data['confirmpassword']){
+                return show_tip(0,'两次密码不一样请重新输入');
+            }
+            $dataF['id'] = $data['id'];
+            $dataF['password'] = getMd5Password($data['newpassword']);
+            if (false !== M('admin')->save($dataF)) {
+                session_unset();
+                session_destroy();
+                return show_tip(1, '修改成功,请重新登陆', null, U('Login/index'));
+            } else {
+                return show_tip(0, '修改失败');
+            }
+        } else {
+            $userid = session('zzcms_adm_userid');
+            $condition = array('id' => $userid);
+            $usernameRes = M('admin')->where($condition)->select();
+            $username = $usernameRes[0]['username'];
+            $this->assign("username", $username);
+            $this->assign("id",$userid);
+            $this->assign("type", "修改密码");
+            $this->display();
+        }
     }
 }
 
